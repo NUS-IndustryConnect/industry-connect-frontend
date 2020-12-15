5e0660f4f7c2b1b16d0bce02520e520b0513f7e2
Wen Jun <wenjun.lye@gmail.com>
2020-12-15 20:54:38 +0800

    Add login page

---
+++ src/admin/pages/Login/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Page from '../../../common/Page';
+import { Link } from 'react-router-dom';
+
+import './index.css';
+
+export default function Login() {
+  return (
+    <Page title="Admin Dashboard">
+      <div className="login">
+        <h3>Welcome to IndustryConnect!</h3>
+        <p>SoC Industry Updates is a platform made by students, for students. It serves as a one-stop shop for students from the School of Computing to learn about internships, jobs and future career opportunities in various industries.</p>
+
+        <Link to="/admin">
+          <button className="primary">Login</button>
+        </Link>
+      </div>
+    </Page>
+  )
+}
