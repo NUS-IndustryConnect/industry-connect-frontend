39e57f8bba7bb91173c8c484f3b44d57f46ac6ff
Wen Jun <wenjun.lye@gmail.com>
2020-12-17 23:02:49 +0800

    Update forms
    Refactor forms into their own files which New.js and Edit.js both share
    Refactor form fields getter functions

--- src/admin/pages/Industry/Users/New.js
+++ src/admin/pages/Industry/Users/New.js
@@ -2,28 +2,18 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 
 import Page from '../../Page';
-import Form from '../../Form';
+import UsersForm, { getUserFields } from './UsersForm';
 
 export default function New() {
   const history = useHistory();
   const submit = data => {
-    const companyObj = {
-      email: data.get('email'),
-      company: data.get('company'),
-    };
+    const companyObj = getUserFields(data);
     console.log(companyObj);
     history.push('/admin/industry/users');
   }
   return (
     <Page title="New Company User">
-      <Form
-        fields={[
-          { type: "email", name: "email", label: "Email Address" },
-          { type: "text", name: "company", label: "Company" },
-        ]}
-        submit={submit}
-        submitLabel="Create"
-      />
+      <UsersForm submit={submit}/>
     </Page>
   )
 }
---
+++ src/admin/pages/Industry/Users/UsersForm.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Form from '../../Form';
+
+export const getUserFields = data => ({
+  email: data.get('email'),
+  company: data.get('company'),
+});
+
+export default function UsersForm({ submit, initial }) {
+  return (
+    <Form
+      fields={[
+        { type: "email", name: "email", label: "Email Address", initial: initial?.email },
+        { type: "text", name: "company", label: "Company", initial: initial?.company },
+      ]}
+      submit={submit}
+      submitLabel={initial ? "Update" : "Create"}
+    />
+  )
+}
