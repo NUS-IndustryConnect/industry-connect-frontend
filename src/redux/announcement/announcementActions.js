33ccdd1cd3e06f6a47cdf66aeaf4cc3cc19f81bb
Wen Jin <Nijnxw@users.noreply.github.com>
2021-01-05 17:39:28 +0800

    Organise redux files for student announcements

--- src/redux/announcement/announcementActions.js
+++ src/redux/announcement/announcementActions.js
@@ -1,5 +1,5 @@
 import { LOAD_STUDENT_ANNOUNCEMENT_DATA } from "../types"
-import { getAnnouncementsForStudents } from "../utils/announcementFunctions"
+import { getAnnouncementsForStudents } from "./announcementFunctions"
 
 export const loadStudentAnnouncementData = () => {
     console.log('Loading Student Announcement Data...')
