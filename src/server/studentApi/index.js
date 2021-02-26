e69305cebd073af6d550c83468acdc8715d4ce95
Wen Jin <Nijnxw@users.noreply.github.com>
2021-02-26 11:45:21 +0800

    Fix import statements

--- src/server/studentApi/index.js
+++ src/server/studentApi/index.js
@@ -2,12 +2,6 @@ import { getAnnouncements } from "./announcements";
 import { getCompanies } from "./companies";
 import { getCompanyPosts } from "./companyPosts";
 
-export const getCompaniesStudent = () => {
-  return api.get("/company/student")
-    .then(response => response.data)
-    .catch(error => { throw error });
-}
-
 const studentApi = {
   getAnnouncements,
   getCompanyPosts,
