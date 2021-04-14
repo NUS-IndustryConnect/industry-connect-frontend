cb4c799383d100f4bca2c53dc2eac2dc95b29316
Wen Jin <Nijnxw@users.noreply.github.com>
2021-04-14 11:57:07 +0800

    Integrate adfs for admin login

--- src/admin/pages/Login/index.js
+++ src/admin/pages/Login/index.js
@@ -1,24 +1,46 @@
 import React from 'react';
-
-import Page from '../../../common/Page';
-import './index.css';
+import { useHistory, useLocation } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import { useHistory } from 'react-router-dom';
-import { loginAdmin } from '../../../redux/user/userActions';
+import ClipLoader from "react-spinners/ClipLoader";
+
+import './index.css';
+import Page from '../../../common/Page';
 import { getAdminIndustryThunk } from '../../../redux/industry';
 import { announcementThunks } from '../../../redux/announcementSlice';
+import { handleFetchAuth, logout } from '../../../redux/user/userActions';
 
 export default function Login() {
   // TODO: link up to authentication (temporary placeholder)
   const dispatch = useDispatch();
   const history = useHistory();
-  
-  const handleLogin = () => {
-    dispatch(loginAdmin()).then(()=> {
-      dispatch(announcementThunks.getAdminAnnouncements());
-      dispatch(getAdminIndustryThunk());
-      history.push("/admin/announcements");
-    })
+  const location = useLocation();
+  let code = location.search.substring(6);
+
+  React.useEffect(() => {
+    if (code) {
+      dispatch(handleFetchAuth(code)).then(() => {
+        dispatch(announcementThunks.getAdminAnnouncements());
+        dispatch(getAdminIndustryThunk());
+        history.push("/admin/announcements");
+      }).catch(error => {
+        dispatch(logout()).then(() => {
+          history.push("/admin/login");
+        })
+      })
+    }
+  }, [code, dispatch, history]);
+
+  if (code) { // buffer to wait for info
+    return (
+      <Page title="Admin Dashboard">
+      <div className="login">
+        <h3>Welcome to IndustryConnect!</h3>
+        <p>SoC Industry Updates is a platform made by students, for students. It serves as a one-stop shop for students from the School of Computing to learn about internships, jobs and future career opportunities in various industries.</p>
+        <ClipLoader color="#003D7C" loading={true} size={50} />
+        <p>Logging in...</p>
+      </div>
+    </Page>
+    )
   }
 
   return (
@@ -27,7 +49,9 @@ export default function Login() {
         <h3>Welcome to IndustryConnect!</h3>
         <p>SoC Industry Updates is a platform made by students, for students. It serves as a one-stop shop for students from the School of Computing to learn about internships, jobs and future career opportunities in various industries.</p>
 
-        <button className="primary" onClick={handleLogin}>Login</button>
+        <a className="primary" href="https://vafs.nus.edu.sg/adfs/oauth2/authorize?response_type=code&client_id=INC000002302194&resource=sg_edu_nus_oauth&redirect_uri=http%3A%2F%2Fwinuat11-i.comp.nus.edu.sg%3A3344%2Fstudent%2Flogin">
+          <button className="primary">Login</button>
+        </a>
       </div>
     </Page>
   )
